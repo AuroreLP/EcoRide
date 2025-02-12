ea83a04bcc5cad0909bda861f65b90a059d36dba
Aurore <aleperff@gmail.com>
2025-02-12 09:38:20 +0100

    config home et ridecontroller + navbar

---
+++ src/Controller/RideController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class RideController extends AbstractController
+{
+    #[Route("/covoiturages", name: "ride.index")]
+    public function index(Request $request): Response
+    {
+        return $this->render("ride/index.html.twig");
+    }
+}
